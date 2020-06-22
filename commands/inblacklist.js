c4542700a99cfe59d14e488b7c2248dcfe1c9417
Niclas Scheel <scheel.niclas@gmail.com>
2020-06-22 11:25:02 +0200

    .

--- commands/inblacklist.js
+++ commands/inblacklist.js
@@ -17,26 +17,25 @@ exports.check_word = (message) => {
               message.client.channels.get("509757254862372883").send(message.author+"hat ein geblacklistetes Wort ("+words[j]+") geschriebenen \n Blacklist")
               return false;
             }
+            if (j == words.length-1 ) {
+              return true;
+            }
           }
         }
 }
 
 exports.check_channel =  (message) => {
   this.message = message;
-  var x
-  console.log(x)
+
   var data = fs.readFileSync('./bannedwords/channelblacklist.txt',"utf8")
     let channelid = data.split("|")
       for (i = 0; i < channelid.length; i++) {
         if (this.message.channel.id==channelid[i]) {
-          console.log(false);
           return false;
         }
         if (i == channelid.length-1) {
-          console.log(true);
           return true;
         }
       }
 
-    console.log(x)
 }
