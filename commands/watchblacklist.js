3532228c84eafa6ebb82086bd97ca2a610e62227
Niclas Scheel <scheel.niclas@gmail.com>
2020-07-01 19:40:19 +0200

    watch blacklistfeature added

---
+++ commands/watchblacklist.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const Discord = require('discord.js');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+
+var transporter = nodemailer.createTransport({
+   host: "smtp.gmail.com",
+  auth: {
+    user: process.env.Mailadress,
+    pass: process.env.Mailpw
+  }
+})
+
+exports.channel = (message) => {
+  if (!message.member.roles.some(role => role.id === "450742960678764544")){
+     this.message.channel.send("Du hast keine Berechtigung dafür")
+     return;
+   }
+  fs.readFile('./bannedwords/blacklist.txt',"utf8",(err, data) => {
+    this.message.channel.send(data)
+  })
+}
+
+exports.word = (message) => {
+  if (!message.member.roles.some(role => role.id === "450742960678764544")){
+     this.message.channel.send("Du hast keine Berechtigung dafür")
+     return;
+   }
+   fs.readFile('./bannedwords/channelblacklist.txt',"utf8",(err, data) => {
+     this.message.channel.send(data)
+   })
+}
