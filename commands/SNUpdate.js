d57a59895c5a306f1bda28bbc6b71a018eae9217
Niclas Scheel <scheel.niclas@gmail.com>
2020-07-01 18:36:04 +0200

    SNUpdate

--- commands/SNUpdate.js
+++ commands/SNUpdate.js
@@ -23,7 +23,7 @@ exports.SNUpdate = (message) => {
   .exec()
   .then(docs => {
     docs.forEach((doc, idx, message) => {
-      let guildMember = this.message.guild.fetchMember(doc.MemberId)
+      let guildMember = this.message.guild.members.get(doc.MemberId)
       if (doc.stammgast == 0){
 
          var cxc1= 0
