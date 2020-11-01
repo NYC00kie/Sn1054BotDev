a453c5f779fea15fb3e1329be0654279b95655b8
Niclas Scheel <scheel.niclas@gmail.com>
2020-11-01 17:15:33 +0100

    tyring to remove TypeError when fetching messeges

--- commands/channel.js
+++ commands/channel.js
@@ -15,21 +15,21 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-async function lots_of_messages_getter(channel, limit = 10000) {
+async function lots_of_messages_getter(channel, limitt = 10000) {
   const sum_messages = [];
   let last_id;
 
   while (true) {
-    const options = { limit: 100 };
+    const options = { limit: 99 };
     if (last_id) {
     options.before = last_id;
     }
-    console.log(options)
+
     const messages = await channel.messages.fetch(options);
     sum_messages.push(messages.size);
     last_id = messages.last().id;
 
-    if (messages.size != 100 || sum_messages >= limit) {
+    if (messages.size != 99 || sum_messages >= limitt) {
       break;
     }
 }
