1d7b261956b545cb5dedd17c7e654e510a075bcc
Niclas Scheel <scheel.niclas@gmail.com>
2020-11-03 16:17:25 +0100

    fixed .start

--- commands/start.js
+++ commands/start.js
@@ -18,7 +18,7 @@ exports.start = (message) => {
   var Community = message.member.guild.roles.cache.find(role => role.id === "518385317229625364");
   var Community_member = message.member.roles.cache.find(role => role.id === "518385317229625364");
   if (Community_member!=Community) {
-    this.message.member.addRole(Community)
+    this.message.member.roles.add("518385317229625364")
     message.client.channels.cache.get("484039225331679253").send(message.author+"hat sich verifiziert \n Verifikation")
     message.client.channels.cache.get("727919338606166096").send(message.author+" Willkommen auf diesem Server")
   }
