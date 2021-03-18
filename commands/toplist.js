bd31fd28b78300210522b46224d431ac5c90531f
Niclas Scheel <scheel.niclas@gmail.com>
2021-03-18 17:47:03 +0100

    .

--- commands/toplist.js
+++ commands/toplist.js
@@ -193,7 +193,7 @@ async function lots_of_messages_getter(channel, limitt = 10000) {
 
 return sum_messages;
 }
-󠀀
+
 async function how_many_messages_are_there_actually(msgcount){
   let PreActuall_messages_v0 = msgcount.toString();
   let Actuall_messages_v0 = PreActuall_messages_v0.split(",");
