5222ff5113b705069836432e487c929e16b7e671
NYC00kie <scheel.niclas@gmail.com>
2021-07-10 14:29:37 +0200

    used id instead of full user tag

--- commands/toplist.js
+++ commands/toplist.js
@@ -28,7 +28,7 @@ exports.nvctop = (message) => {
   var TopEmbed = new Discord.MessageEmbed()
     .setColor(0xe19517)
     .setTitle("Die 10 Leute mit den meisten nvc")
-    .addField("឵឵ ឵឵ ឵឵","1. "+ docs[0].MemberId+" mit "+docs[0].cxc+"\n\n2. "+ docs[1].MemberId+" mit "+docs[1].cxc+"\n\n3. "+ docs[2].MemberId+" mit "+docs[2].cxc+"\n\n4. "+ docs[3].MemberId+" mit "+docs[3].cxc+"\n\n5. "+ docs[4].MemberId+" mit "+docs[4].cxc+"\n\n6. "+ docs[5].MemberId+" mit "+docs[5].cxc+"\n\n7. "+ docs[6].MemberId+" mit "+docs[6].cxc+"\n\n8. "+ docs[7].MemberId+" mit "+docs[7].cxc+"\n\n9. "+ docs[8].MemberId+" mit "+docs[8].cxc+"\n\n10. "+ docs[9].MemberId+" mit "+docs[9].cxc)
+    .addField("឵឵ ឵឵ ឵឵","1. "+ docs[0].Name+" mit "+docs[0].cxc+"\n\n2. "+ docs[1].Name+" mit "+docs[1].cxc+"\n\n3. "+ docs[2].Name+" mit "+docs[2].cxc+"\n\n4. "+ docs[3].Name+" mit "+docs[3].cxc+"\n\n5. "+ docs[4].Name+" mit "+docs[4].cxc+"\n\n6. "+ docs[5].Name+" mit "+docs[5].cxc+"\n\n7. "+ docs[6].Name+" mit "+docs[6].cxc+"\n\n8. "+ docs[7].Name+" mit "+docs[7].cxc+"\n\n9. "+ docs[8].Name+" mit "+docs[8].cxc+"\n\n10. "+ docs[9].Name+" mit "+docs[9].cxc)
     .setFooter("Heute ist der: "+Date("now"))
     this.message.channel.send(TopEmbed)
 
