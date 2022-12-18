d123d99c9ab1f81478341d569bf84fca80357e05
Nici Scheel <scheel.niclas@gmail.com>
2022-12-18 18:59:10 +0100

    re rewrote buyrole

--- slashcommands/buyrole.js
+++ slashcommands/buyrole.js
@@ -1,4 +1,6 @@
 const {EmbedBuilder,SlashCommandBuilder} = require('discord.js');
+const mongoose = require('mongoose');
+const Sale = require('../models/sale');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,22 +14,22 @@ module.exports = {
 	async execute(interaction) {
 		const selectedrole = interaction.options.getRole('role')
 		console.log(selectedrole)
-		interaction.reply({content:`selected ${selectedrole}`,ephemeral:false})
+		await interaction.deferReply( {ephemeral: false} );
 
-		const Author = this.message.author
+		const Author = interaction.member
 		const MemberID = interaction.member.id
 		const Search = selectedrole.name
 		Sale.findOne({
 				MemberId: MemberID
 			})
 			.exec()
-			.then(docs => {
+			.then(async docs => {
 				if (Search == "Memes") {
 					if (docs.memes == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 50) {
 						interaction.member.roles.add("518395092197965845")
-						interaction.reply({content:"Du hast die Rolle für: **50nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **50nvc** gekauft")
 						Sale.updateOne({
 								_id: docs._id
 							}, {
@@ -41,15 +43,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518395092197965845", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Gaswolke") {
 					if (docs.stammgast == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 3750) {
-						interaction.reply({content:"Du hast die Rolle für: **3750nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **3750nvc** gekauft")
 						interaction.member.roles.add("518395091472089101")
 						Sale.updateOne({
 								_id: docs._id
@@ -64,15 +66,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518395091472089101", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Brauner-Zwerg") {
 					if (docs.Bohr == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 6000) {
-						interaction.reply({content:"Du hast die Rolle für: **6000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **6000nvc** gekauft")
 						interaction.member.roles.add("518384555007148042")
 						Sale.updateOne({
 								_id: docs._id
@@ -87,15 +89,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518384555007148042", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Roter-Zwerg") {
 					if (docs.Curie == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 7500) {
-						interaction.reply({content:"Du hast die Rolle für: **7500nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **7500nvc** gekauft")
 						interaction.member.roles.add("518384549408014343")
 						Sale.updateOne({
 								_id: docs._id
@@ -110,15 +112,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518384549408014343", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Weißer-Zwerg") {
 					if (docs.Tesla == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 13000) {
-						interaction.reply({content:"Du hast die Rolle für: **13000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **13000nvc** gekauft")
 						interaction.member.roles.add("518384546492973056")
 						Sale.updateOne({
 								_id: docs._id
@@ -133,15 +135,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518384546492973056", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Hauptreihenstern") {
 					if (docs.Newton == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 20000) {
-						interaction.reply({content:"Du hast die Rolle für: **20000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **20000nvc** gekauft")
 						interaction.member.roles.add("518384356222566410")
 						Sale.updateOne({
 								_id: docs._id
@@ -156,15 +158,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518384356222566410", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Roter-Riese") {
 					if (docs.Einstein == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 25000) {
-						interaction.reply({content:"Du hast die Rolle für: **25000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **25000nvc** gekauft")
 						interaction.member.roles.add("518384355698278430")
 						Sale.updateOne({
 								_id: docs._id
@@ -179,15 +181,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518384355698278430", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Supernova") {
 					if (docs.Hawking == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 50000) {
-						interaction.reply({content:"Du hast die Rolle für: **50000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **50000nvc** gekauft")
 						interaction.member.roles.add("518384354880258049")
 						Sale.updateOne({
 								_id: docs._id
@@ -202,15 +204,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518384354880258049", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Neutronen-Stern") {
 					if (docs.Musk == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 75000) {
-						interaction.reply({content:"Du hast die Rolle für: **75000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **75000nvc** gekauft")
 						interaction.member.roles.add("518384354272215060")
 						Sale.updateOne({
 								_id: docs._id
@@ -225,15 +227,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518384354272215060", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Schwarzes-Loch") {
 					if (docs.Vip == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 80000) {
-						interaction.reply({content:"Du hast die Rolle für: **80000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **80000nvc** gekauft")
 						interaction.member.roles.add("518384353387085845")
 						Sale.updateOne({
 								_id: docs._id
@@ -248,15 +250,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518384353387085845", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Pulsar") {
 					if (docs.Clixoomer == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 95000) {
-						interaction.reply({content:"Du hast die Rolle für: **95000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **95000nvc** gekauft")
 						interaction.member.roles.add("518172524811386890")
 						Sale.updateOne({
 								_id: docs._id
@@ -271,15 +273,15 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "518172524811386890", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
 				else if (Search == "Quasar") {
 					if (docs.quasar == 1) {
-						interaction.reply({content:"Du hast diese Rolle schon",ephemeral:true})
+						interaction.editReply("Du hast diese Rolle schon")
 					} else if (docs.cxc >= 115000) {
-						interaction.reply({content:"Du hast die Rolle für: **115000nvc** gekauft",ephemeral:true})
+						interaction.editReply("Du hast die Rolle für: **115000nvc** gekauft")
 						interaction.member.roles.add("700740599195893760")
 						Sale.updateOne({
 								_id: docs._id
@@ -294,12 +296,12 @@ module.exports = {
 								Loghandler.log(message, Author, undefined, "buyrole", "700740599195893760", undefined)
 							})
 					} else {
-						interaction.reply({content:"Du hast nicht genug nvc",ephemeral:true})
+						interaction.editReply("Du hast nicht genug nvc")
 					}
 
 				}
-				else if (Search == undefined) {
-					this.message.channel.send("please define a Role you want to buy")
+				else {
+					await interaction.editReply("Etwas ist schief gegangen.\nbitte definiere eine Rolle, die du kaufen kannst")
 				}
 			})
 
